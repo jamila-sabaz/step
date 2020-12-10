c27919426c6c5f004b88e92d3eb354e85f900da2
Jamila Sabazova <zh.sabazova02@gmail.com>
2020-12-10 08:30:52 +0000

    fix slideshow

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,48 +15,107 @@
 /**
  * Adds a random greeting to the page.
  */
+showHasBeenClicked = false;
+factHasBeenClicked = false;
 function recommendRandomShow() {
   const shows =
-      ['The Good Place', 'Gossip Girl', 'Community', 'Emily in Paris'];
-
-  // Pick a random greeting.
+    ['The Good Place', 'Gossip Girl', 'Community', 'Emily in Paris', 'How I met Your Mother'];
+    
+  // Pick a random tv show.
   const show = shows[Math.floor(Math.random() * shows.length)];
 
   // Add it to the page.
   const showContainer = document.getElementById('tv-show-container');
-  showContainer.innerText = show;
+  
+  if (showHasBeenClicked === true){
+    showContainer.innerText = null;
+    showHasBeenClicked = false;
+  }
+    
+  else{
+      showContainer.innerText = show;
+      showHasBeenClicked = true;
+  }
+    
 }
-
 /**
  * Adds a random fact to the page.
  */
 function randomFact() {
   const facts =
-      ['favourite color - Purple', 'prefers dogs over cats', 'favourite ice cream flavor - strawberry', 
-      'favourite singer - Lady Gaga', 'still dreams to become an astronauts', '90% of times remembers her dreams'];
+    ['favourite color - Purple', 'prefers dogs over cats', 'favourite ice cream flavor - strawberry', 
+    'favourite singer - Lady Gaga', 'still dreams to become an astronauts', '90% of times remembers her dreams'];
 
-  // Pick a random greeting.
+  // Pick a random fact.
   const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
   const factContainer = document.getElementById('fact-container');
-  factContainer.innerText = fact;
-}
-
-
-
-// Automatic Slideshow - change image every 3 seconds
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+    if (factHasBeenClicked === true){
+      factContainer.innerText = null;
+      factHasBeenClicked = false;
+    }
+  else{
+      factContainer.innerText = fact;
+      factHasBeenClicked = true;
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);
 }
+//Button
+function openSection(id) {
+  var section = document.getElementById(id);
+  if (section.className.indexOf("w3-show") == -1) {
+    section.className += " w3-show";
+  } else {
+    section.className = section.className.replace(" w3-show", "");
+  } 
+}
+/**
+ * Automatic Slideshow - change image every 3 seconds
+ */
+document.addEventListener('DOMContentLoaded', () => {
+showSlides(0);
+});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+/*
+document.addEventListener('DOMContentLoaded',(event) => {
+showSlides(0);
+});
+
+function showSlides(slideIndex) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}*/

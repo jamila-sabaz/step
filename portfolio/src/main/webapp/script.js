e8bb10944870ab37e9292ae1d203c554836297c3
Jamila Sabazova <zh.sabazova02@gmail.com>
2020-12-16 03:54:48 +0000

    fix indentation in js file with getServerData function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -129,9 +129,9 @@ function showSlides(n) {
  */
 function getServerData() {
   fetch('/data')
-  .then(response => response.text())
-  .then( (myJSON) => document
-  .getElementById('data-container')
-  .innerText = myJSON );
+    .then(response => response.text())
+      .then( (myJSON) => document
+        .getElementById('data-container')
+          .innerText = myJSON );
 }
 
